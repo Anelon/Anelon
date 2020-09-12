cf708314ebd503ee973abeca5225ff4106e1db3f
Andrew Bell <majacow@gmail.com>
2020-09-11 17:16:12 -0700

    stated update script

---
+++ update.ts
@@ -0,0 +1,9 @@
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { Dwitter } from "https://deno.land/x/dwitter/mod.ts";
+
+const twitterAPIKeys = config({ path: "./.twitter.env" });
+
+const dwitter = new Dwitter(twitterAPIKeys);
+
+console.log(await dwitter.getTweet("1275456813354401794"));
+
